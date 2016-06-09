6c807c7cb0ea3611f4fa6c1e14b0896a9bf24362
Vladimir Kuzmichev <kuzmichev.vladimir@gmail.com>
2016-06-09 20:05:17 +0300

    Initial commit.

---
+++ TrinityDemoSwift/Controllers/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TrinityDemoSwift
+//
+//  Created by Vladimir Kuzmichev on 09.06.16.
+//  Copyright © 2016 Vladimir Kuzmichev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
